422a21e4dd36976408170209983361f8e0d37452
k3rcaft <vqt.develop@gmail.com>
2024-01-21 17:21:53 +0700

    add new sample project: multi-projects

---
+++ maven/multi-projects/application/src/main/java/com/bigcat3997/application/DemoApplication.java
@@ -0,0 +1,28 @@
+package com.bigcat3997.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.bigcat3997.library.service.MyService;
+
+@SpringBootApplication(scanBasePackages = "com.bigcat3997.library")
+@RestController
+public class DemoApplication {
+
+	private final MyService myService;
+
+	public DemoApplication(MyService myService) {
+		this.myService = myService;
+	}
+
+	@GetMapping("/")
+	public String home() {
+		return myService.message();
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+}
